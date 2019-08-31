6f94ce336ea4c9416bb40ea23a6d39ebad7d9c0c
Reuben John <rjkoppy@gmail.com>
2019-08-31 15:44:44 -0500

    Add application model

--- job_apps/models.py
+++ job_apps/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Application(models.Model):
+    '''An application the user has submitted'''
+    STATUSES = [('submitted', 'SUBMITTED'),
+                ('accepted', 'ACCEPTED'),
+                ('rejected', 'REJECTED')]
+    position = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    post_url = models.CharField(max_length=300)
+    contact_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUSES)
--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'job_apps',
+
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
