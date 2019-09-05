4b4b80c50664972e3c39517e192d25fb6820849d
Reuben John <rjkoppy@gmail.com>
2019-09-05 10:56:41 -0500

    Add bootstrap4

--- job_tracker/settings.py
+++ job_tracker/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'job_apps',
     'users',
 
+    # Third party apps
+    'bootstrap4',
+
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
